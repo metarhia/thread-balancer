f1008f04884b45894aa2169d2172693a24d434ae
Vitalii Kukhar <kukhar.vitalii@gmail.com>
2019-09-11 16:24:44 +0300

    clear

--- server/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  ports: [3000, 3001, 3002, 3003],
-  hostname: '127.0.0.1',
-  spawnerName: 'processes', //processes, threads
-  transportName: 'tcp', // http, tcp, udp
-};
--- transport/udp.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = (serverModule, options) => {
-  console.log({ serverModule, options });
-};
