8074de59a9a5533cbfe774da109fce600345e692
Vitalii Kukhar <kukhar.vitalii@gmail.com>
2019-11-05 13:54:46 +0200

    server cpu&memory logging

--- server/master.js
+++ server/master.js
@@ -1,10 +1,15 @@
 'use strict';
 const path = require('path');
+const fs = require('fs');
+const { cpuFree, freememPercentage } = require('os-utils');
+
 const configPath = path.join(__dirname, 'config.js');
 const { ports, hostname, spawnerName, disconnect } = require(configPath);
 
 const workerPath = path.join(__dirname, 'worker.js');
 const spawnerPath = path.join(__dirname, 'spawner', `${spawnerName}.js`);
+const logCPU = path.join(__dirname, 'log', 'cpu.log');
+const logMemory = path.join(__dirname, 'log', 'memory.log');
 
 const { create } = require(spawnerPath);
 
@@ -15,6 +20,7 @@ const portsDisconnect = ports
   .slice(0, disconnect);
 const getRandom = (min, max) => Math.random() * (max - min) + min;
 
+// run servers
 ports.forEach(port => {
   const isDisconnect = portsDisconnect.indexOf(port) !== -1;
   const worker = create(workerPath, [port, hostname, isDisconnect]);
@@ -30,3 +36,18 @@ ports.forEach(port => {
     );
   });
 });
+
+// get CPU and memory usage
+setInterval(() => {
+  cpuFree(v => {
+    logger(logCPU, `${(v * 100).toFixed(0)},`);
+    logger(logMemory, `${(freememPercentage() * 100).toFixed(0)},`);
+  });
+}, 1000);
+
+// logger
+function logger(file, data) {
+  const ws = fs.createWriteStream(file, { flags: 'a' }, 'utf-8');
+  ws.write(data);
+  ws.end();
+}
